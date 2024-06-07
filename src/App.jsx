0de47f5cad7dd4752b27c3fb2537316238e2e3bb
Corbin <140107765+Promingy@users.noreply.github.com>
2024-06-07 14:41:50 -0600

    added flame component - trying to get multiple flames animated - running into issues with it. out side of this I just have to import the rest of my models and set up the camera movements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Canvas, useThree } from '@react-three/fiber'
 import React, { useEffect, useRef } from 'react'
+import { OrbitControls } from '@react-three/drei'
 import './App.css'
 
 import LoadModel from './components/LoadModel'
@@ -8,24 +9,11 @@ import Flame from './components/Flame/Flame'
 
 const url = 'https://glb-bucket-portfolio.s3-accelerate.amazonaws.com/'
 
-// const CameraSetup = () => {
-//   const { camera } = useThree()
-
-//   useEffect(() => {
-//     // Set camera default position
-//     camera.position.set(-200, 175, 200)
-
-//     // Set the camera default lookAt point
-//     camera.lookAt(0, 0, 0)
-// }, [camera])
-
-//   return null
-// }
 
 const App = () => {
   return (
     <Canvas shadows camera={{ position: [-200, 175, 200]}} style={{ background: '#272727' }}>
-      {/* <CameraSetup /> */}
+      <OrbitControls />
 
       <group>
         <Lights position={[44, 50, 80]}  intensity={2000} />
@@ -45,7 +33,22 @@ const App = () => {
       </group>
 
       <LoadModel url={url + 'updated_tavern'} scale={[25, 25, 25]}/>
-      <Flame url={url + 'animated_flame1'} />
+      <group>
+        <Flame url={url + 'animated_torch_flame1'} position={[-34, 7, -70]} scale={[13, 5, 10]}/>
+
+        <Flame url={url + 'animated_torch_flame1'} position={[49, 53, 79]} scale={[4.5, 1.5, 4.5]}/>
+        <Flame url={url + 'animated_torch_flame1'} position={[49, 53, -30]} scale={[4.5, 1.5, 4.5]}/>
+        <Flame url={url + 'animated_torch_flame1'} position={[-26, 53, -66]} scale={[4.5, 1.5, 4.5]}/>
+
+        <Flame url={url + 'animated_torch_flame1'} position={[65, 63, 120]} scale={[4.5, 1.5, 4.5]}/>
+        <Flame url={url + 'animated_torch_flame1'} position={[65, 63, -70]} scale={[4.5, 1.5, 4.5]}/>
+        <Flame url={url + 'animated_torch_flame1'} position={[53, 63, -83]} scale={[4.5, 1.5, 4.5]}/>
+        <Flame url={url + 'animated_torch_flame1'} position={[-90, 63, -83]} scale={[4.5, 1.5, 4.5]}/>
+
+        {/* <Flame url={url + 'animated_torch_flame1'} position={[-34, 7, -70]} scale={[10, 10, 10]}/> */}
+        {/* <Flame url={url + 'animated_torch_flame1'} position={[-34, 7, 70]} scale={[10, 10, 10]}/> */}
+      </group>
+
     </Canvas>
   )
 }
--- src/components/Flame/Flame.jsx
+++ src/components/Flame/Flame.jsx
@@ -1,13 +1,58 @@
-import { useFrame, useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/Addons.js";
+// import { useLoader } from "@react-three/fiber";
+import { useRef, useEffect, useMemo } from "react";
+import { useAnimations, useGLTF } from "@react-three/drei";
+import * as Three from 'three'
+import { useFrame } from "@react-three/fiber";
 
 export default function  Flame(props) {
-    const gltf = useLoader(GLTFLoader, props.url  + '.glb');
-    const ref = useRef();
+    const { scene, animations } = useGLTF(props.url + '.glb');
+    const flameRef = useRef();
+    // const { actions } = useAnimations(animations, ref)
+    const mixerRef = useRef();
+    useEffect(() => {
+        if (!scene || !animations) return;
 
-     useFrame((state, delta) => {
-        ref.current
-    })
+        mixerRef.current = new Three.AnimationMixer(scene);
 
-    return null
+        animations.forEach(child => {
+            const action = mixerRef.current.clipAction(child);
+            action.play().setLoop(Three.LoopRepeat);
+        })
+
+        return () => mixerRef.current.stopAllAction();
+    // play animations
+        // const actions = animations.map(animation => {
+        //     const action = mixer.clipAction(animation, flameRef.current);
+        //     action.play();
+        //     return action;
+        // });
+
+        // if (actions) {
+        //     Object.values(actions).forEach(action => {
+        //         action.play().setLoop(Three.LoopRepeat);
+        //     })
+        // }
+
+        // return () => MixOperation.stopAllAction();
+
+    }, [animations, scene]);
+
+    useFrame((state, delta) => {
+        if (mixerRef.current) {
+            mixerRef.current.update(delta);
+        }
+    });
+
+    return (
+        <primitive 
+        object={scene}
+        ref={flameRef}
+        position={props.position || [0, 0, 0]}
+        rotation={props.rotation || [0, 0, 0]}
+        scale={props.scale || [1, 1, 1]}
+        castShadow
+        receiveShadow
+        />
+
+    )
 }
