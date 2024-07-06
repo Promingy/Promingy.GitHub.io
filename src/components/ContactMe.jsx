242a03b053d43896dcb065b9535ae7d061b895be
Corbin <140107765+Promingy@users.noreply.github.com>
2024-07-06 15:56:58 -0600

    added clouds to contact form

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,4 +1,4 @@
-import { Detailed, Html, TransformControls, useGLTF } from '@react-three/drei'
+import { Cloud, Clouds, Detailed, Float, Html, TransformControls, useGLTF } from '@react-three/drei'
 import { MeshBasicMaterial } from 'three'
 import { useAppContext } from '../context'
 import { useFrame, useThree } from '@react-three/fiber'
@@ -18,6 +18,9 @@ export default function Contact(props) {
   const [ success, setSuccess ] = useState("");
   const [ error, setError ] = useState("");
 
+  const [ opacity, setOpacity ] = useState(0);
+  const [ hovered, setHovered ] = useState(false);
+
   const context = useAppContext();
   const { controls } = useThree();
 
@@ -25,12 +28,11 @@ export default function Contact(props) {
   const midBasic = new MeshBasicMaterial({map: midMats.T_floor_sign_1001.map})
   const highBasic = new MeshBasicMaterial({map: highMats.T_floor_sign_1001.map})
 
-  // useFrame(() => {
-  //   if (ref.current){
-  //     ref.current.updateMatrixWorld();
-  //     console.log(ref.current.rotation)
-  //   }  
-  // })
+  useFrame(() => {
+    if (context.lookingAt == 'none' && hovered && opacity < 1) setOpacity(opacity + 0.0025)
+    else if (opacity > 0) setOpacity(opacity - 0.00075)
+  
+  })
 
   function onSubmit(e){
     e.preventDefault();
@@ -69,6 +71,14 @@ export default function Contact(props) {
 
   return (
     <>
+    {opacity && 
+      <Clouds material={MeshBasicMaterial}>
+        <Cloud opacity={opacity} speed={1} seed={.26} scale={2} position={[-92, -5, 115]} rotation={[0.75, 0, 0]}/>
+        <Cloud opacity={opacity} speed={1} seed={.26} scale={2} position={[-80, -5, 110]} rotation={[0.75, 0, 0]}/>
+        <Cloud opacity={opacity} speed={1} seed={.26} scale={2} position={[-80, -5, 120]} rotation={[0.75, 0, 0]}/>
+        <Cloud opacity={opacity} speed={1} seed={.26} scale={2} position={[-87, -5, 123]} rotation={[0.75, 0, .75]}/>
+      </Clouds>
+    }
     {context.lookingAt == 'contact' &&
     <>
     <group ref={ref} scale={[1.9, 2, 1]} position={[-87, 9, 116.5]} rotation={[-0.224, -0.796, -0.139]}>
@@ -117,8 +127,11 @@ export default function Contact(props) {
     </group>
     </>
     }
-      {/* <Detailed {...props} distances={[0, 50, 100]} onPointerOver={context.handlePointerIn} onPointerOut={context.handlePointerOut} > */}
-      <Detailed {...props} distances={[0, 50, 100]} onPointerOver={context.handlePointerIn} onPointerOut={context.handlePointerOut} onClick={e => context.handleClick(e, controls, props)}>
+      <Detailed {...props} 
+        distances={[0, 50, 100]} 
+        onPointerOver={(e) => {context.handlePointerIn(e); setHovered(true)}} 
+        onPointerOut={(e) => {context.handlePointerOut(e); setHovered(false)}} 
+        onClick={e => context.handleClick(e, controls, props)}>
           <mesh geometry={nodes.defaultMaterial.geometry} material={highBasic} position={[0, 56.743, 0.001]} rotation={[-Math.PI / 2, 0, 0]} scale={56.743} />
           <mesh geometry={nodes.defaultMaterial.geometry} material={midBasic} position={[0, 56.743, 0.001]} rotation={[-Math.PI / 2, 0, 0]} scale={56.743} />
           <mesh geometry={nodes.defaultMaterial.geometry} material={lowBasic} position={[0, 56.743, 0.001]} rotation={[-Math.PI / 2, 0, 0]} scale={56.743} />
