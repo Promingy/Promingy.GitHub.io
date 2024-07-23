4008e4c96723895bed6e100cb1c73e5a350a5bd4
Corbin <140107765+Promingy@users.noreply.github.com>
2024-07-23 17:59:16 -0600

    added tavern.tsx - utilized npx gltfjsx -t, had to make a few changes. depending on how well this translates I should be able to use that for all of the models

---
+++ src/tsx_components/Tavern.tsx
@@ -0,0 +1,24 @@
+import { Mesh, MeshBasicMaterial } from 'three'
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+import { GLTF } from 'three-stdlib'
+
+type GLTFResult = GLTF & {
+  nodes: {
+    ['Piso_Surface_-_Matte_0001']: Mesh
+  }
+  materials: {
+    MasterAtlas: MeshBasicMaterial
+  }
+}
+
+export function Model(props: JSX.IntrinsicElements['group']) {
+  const { nodes, materials } = useGLTF('models/tavern.glb') as GLTFResult
+  return (
+    <group {...props} dispose={null}>
+      <mesh castShadow geometry={nodes['Piso_Surface_-_Matte_0001'].geometry} material={materials.MasterAtlas} position={[-0.353, 1.531, 0.611]} rotation={[-Math.PI, 0, -Math.PI]} scale={4.426} />
+    </group>
+  )
+}
+
+useGLTF.preload('models/tavern.glb')
